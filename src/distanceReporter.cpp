3ed6ffbcdb337536321b665e83b0e80ff58c7077
MateiMarica <matei.marica@outlook.com>
2021-01-15 19:37:45 -0400

    Quality of life changes
    * distanceReporter now reports distances in mm over cm for keeping precision while still transmitting integers.
    * getAutoThreshold() now restarts if distances are not steady.
    * Prompts user if manually-entered threshold doesn't make sense.
    * General commenting
    * Added buffer reseting at various stages of program

--- src/distanceReporter.cpp
+++ src/distanceReporter.cpp
@@ -6,41 +6,45 @@
 // This pin connects to the echo pin of the HC-SR04 distance sensor.
 #define ECHO_PIN 8
 
-long microsecondsToCentimeters(long duration);
+long microsecondsToMillimeters(long duration);
 
 // Function runs once at beginning.
 void setup() {
 	pinMode(TRIGGER_PIN, OUTPUT);
 	pinMode(ECHO_PIN, INPUT);
 	Serial.begin(9600);
+	
 }
 
 // Function loops infinitely.
 void loop() {
-	
+
 	// Clear the TRIGGER_PIN state.
 	digitalWrite(TRIGGER_PIN, LOW);
 	delayMicroseconds(10);
 
-	// The sensor is triggered by a HIGH pulse of 10 or more microseconds. Using 1 millisecond to be safe.
+	// The sensor is triggered by a HIGH pulse of 10 or more microseconds.
 	digitalWrite(TRIGGER_PIN, HIGH);
 	delayMicroseconds(10);
 
 	// The duration between transmitting the ultrasonic signal and recieving the echo.
 	long duration = pulseIn(ECHO_PIN, HIGH);
 
-	//The distance travelled in the given duration in centimeters.
-	int centimeters = microsecondsToCentimeters(duration);
+	// The distance travelled in the given duration in centimeters.
+	int millimeters = microsecondsToMillimeters(duration);
 	
-	Serial.println(centimeters);
+	// This is what sends the distance to the receiver.py program.
+	Serial.println(millimeters);
 
 	delay(25);
 }
 
-// Converts the distance travelled in the given duration of microseconds to cm.
-// The speed of sound is 340 m/s or 29 microseconds/cm.
-// duration is already in microseconds, so divide by 29.
-// Then divide by 2 since to get half the distance travelled (distance between sensor to object).
-long microsecondsToCentimeters(long duration) {
-	return duration / 29 / 2;
+/*
+	Converts the distance travelled in the given duration of microseconds to mm.
+	The speed of sound is ~343 m/s or 2.92 microseconds/mm.
+	duration is already in microseconds, so divide by 2.92.
+	Then divide by 2 since to get half the distance travelled (distance between sensor to object).
+*/
+long microsecondsToMillimeters(long duration) {
+	return duration / 2.92 / 2;
 }
--- src/receiver.py
+++ src/receiver.py
@@ -18,39 +18,54 @@ def main():
 	# Keyboard object
 	keyboard = Controller() 
 
+	# Serial object
 	ser = serial.Serial()
 	ser.baudrate = BAUDRATE
 	ser.port = PORT
+	ser.timeout = TIMEOUT
 
-	ser.open()
+	# Tries to open serial port. If failed to open, allows user to retry.
+	while(True):
+		try:
+			ser.open()
+			break
+		except serial.serialutil.SerialException:
+			print("Couldn't open the %s port for serial communication. Is your Arduino connected properly?" % PORT)
+			input("\tPress ENTER to retry...")
 
-	AUTO_THRESHOLD = input("Automatically get threshold? (y/n) ")
-	if AUTO_THRESHOLD == "y":
-		AUTO_THRESHOLD = True
-	else:
-		AUTO_THRESHOLD = False
 
+	# The threshold is the minimum distance at which the safety mechanism isn't triggered.
+	AUTO_THRESHOLD = (input("Automatically get threshold? (y/n) ") == "y")
 	if(AUTO_THRESHOLD):
 		THRESHOLD_DISTANCE = getAutoThreshold(ser)
 	else: 
-		# If something is within this distance, the defense system is executed.
-		THRESHOLD_DISTANCE = float(input("Enter a threshold distance in cm: "))
+		while(True):
+			# If something is within this distance, the defense system is executed.
+			THRESHOLD_DISTANCE = float(input("Enter a threshold distance in cm: "))
+
+			# Checks if the user's threshold is already breached and offers a retry.
+			ser.reset_input_buffer()
+			testDistance = float(re.findall("\d+", str(ser.readline()))[0]) / 10
+			if(THRESHOLD_DISTANCE > testDistance):
+				if(input("There is already an object closer than %.1fcm. Do you want to proceed? (y/n) " % THRESHOLD_DISTANCE) == "y"):
+					break
+				continue # Restarts loop
+			break 			
 	
+	# If the user wants to constantly see the distances
+	SHOW_DISTANCES = (input("\nShow distances in real time? (y/n) ") == "y")
 
-	SHOW_DISTANCES = input("\nShow distances in real time? (y/n) ")
-	if SHOW_DISTANCES == "y":
-		SHOW_DISTANCES = True
-	else:
-		SHOW_DISTANCES = False
-
-	print("\nReceiver activated. Press CTRL + C to quit.")
+	print("\nReceiver activated. You are now safe. Press CTRL+C to quit.")
 
 	try:
+		# Clear any 
+		ser.reset_input_buffer()
+
 		# Infinite loop, reads serial data forever
 		while(True):
 
 			# Must extract int from the received string, since, example: 84 is received as b'84\r\n'
-			distance = int(re.findall("\d+", str(ser.readline()))[0]) 
+			distance = float(re.findall("\d+", str(ser.readline()))[0]) / 10
 
 			if(distance < THRESHOLD_DISTANCE):
 				showDesktop(keyboard)
@@ -59,7 +74,8 @@ def main():
 				print("\tObject at distance of " + str(distance) + "cm")	
 
 	except KeyboardInterrupt:
-		print("Receiver stopped.")
+		ser.close()
+		print("Receiver stopped. You are no longer safe.")
 
 
 # The WINDOWS + M shortcut displays the desktop
@@ -74,19 +90,64 @@ def showDesktop(keyboard):
 def getAutoThreshold(ser):
 	print("Retrieving appropriate threshold...");
 
-	NUM_TESTS = 5
+	# The number of distance tests that will be done to get an average distance.
+	# The higher this is, the more precise the average distance
+	NUM_TESTS = 10
+
+	# Time between tests, in seconds. Example: If NUM_TESTS = 10, getting threshold will be 1 second.
+	# Unused
+	#TIME_BTWN_TESTS = 0.1
+
+	# The percent reduction of the average distance to get the threshold.
+	# The closer to zero, the more sensitive the trigger will be.
 	PERCENT_REDUCTION = 5
 
+	PERCENT_UNDER = ((100 - PERCENT_REDUCTION) / 100) # Example: 0.95 if PERCENT_REDUCTION = 5
+	PERCENT_OVER = ((100 + PERCENT_REDUCTION) / 100)  # Example: 1.05 if PERCENT_REDUCTION = 5
+
 	sum = 0 # Sum of distances
+	lastDistanceCm = None # The last measured distance
+	ser.reset_input_buffer() # Clear any old values in the buffer
 
-	for _ in range(NUM_TESTS):
-		sum += int(re.findall("\d+", str(ser.readline()))[0])
+	i = 0 # Counter variable
+	retries = 0 # The number of times the threshold-getting has failed
 
+	while(i < NUM_TESTS):
+
+		currentDistanceCm = float(re.findall("\d+", str(ser.readline()))[0]) / 10
+		
+
+		# If the current distance is too different than the previous distance, restart the whole process
+		if((lastDistanceCm is not None) and
+			(currentDistanceCm * PERCENT_UNDER > lastDistanceCm or 
+			 currentDistanceCm * PERCENT_OVER < lastDistanceCm)):
+
+			# Reset everything
+			ser.reset_input_buffer()
+			i = 0
+			sum = 0
+			lastDistanceCm = None
+			retries += 1
+
+			# Lets the user know that they suck
+			if(retries % 2 == 0):
+				print("Can't get a steady reading. Retrying...")
+
+			continue
+		
+		lastDistanceCm = currentDistanceCm	# Save the current distance
+		sum += currentDistanceCm			# Adds to sum
+		i += 1								# Increment count
+
+		#time.sleep(TIME_BTWN_TESTS)			# Wait between tests. Unused
+
+	# Get average distance
 	average = sum / NUM_TESTS
 	
-	threshold = average * ((100 - PERCENT_REDUCTION) / 100)
+	# Get an appropriate threshold for the current distance
+	threshold = average * PERCENT_UNDER
 
-	print("Average distance over %d tries was %dcm. Threshold set at %d%% lower, at %.1fcm" 
+	print("Average distance over %d tries was %.1fcm. Threshold set at %d%% lower, at %.1fcm" 
 		% (NUM_TESTS, average, PERCENT_REDUCTION, threshold))
 
 	return threshold
